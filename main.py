f934de2cc2570e9b8c621e2c87771d711894cc07
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2022-01-02 10:35:32 +0530

    adding highscore record through file.txt This reverts commit c2809868ef6621108fb3f75a23a0bec4fac2e5f5.

--- main.py
+++ main.py
@@ -33,15 +33,16 @@ while game_is_on:
         scoreboard.increase_score()
 
     # Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail.
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -5,6 +5,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("yellow")
         self.penup()
         self.goto(0, 270)
@@ -12,13 +14,21 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"score:{self.score} ", align="center", font=("Arial", 20, "normal"))
+        self.clear()
+        self.write(f"score:{self.score}  HighScore: {self.high_score}", align="center", font=("Arial", 20, "normal"))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
         
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Arial", 30, "normal"))
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align="center", font=("Arial", 30, "normal"))
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
